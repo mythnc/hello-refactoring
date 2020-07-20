cee2cc6567574ad0da379db606a5aa9193e2216b
N.C Wu <mythnc@gmail.com>
2020-07-21 01:47:56 +0900

    Delete play parameter

--- ch1/theatrical-players.js
+++ ch1/theatrical-players.js
@@ -34,7 +34,7 @@ function statement(invoice, plays) {
       minimumFractionDigits: 2 }).format;
 
   for (let perf of invoice.performances) { 
-    let thisAmount = amountFor(perf, playFor(perf));
+    let thisAmount = amountFor(perf);
 
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
@@ -51,7 +51,7 @@ function statement(invoice, plays) {
   return result;
 }
 
-function amountFor(aPerformance, play) {
+function amountFor(aPerformance) {
   let result = 0;
   switch (playFor(aPerformance).type) {
     case "tragedy":
