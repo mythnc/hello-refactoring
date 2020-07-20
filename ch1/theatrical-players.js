bdb30cafaac97787fc2a65b6710910842c937554
N.C Wu <mythnc@gmail.com>
2020-07-21 06:42:13 +0900

    Removing the format Variable

--- ch1/theatrical-players.js
+++ ch1/theatrical-players.js
@@ -29,19 +29,15 @@ function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`; 
-  const format = new Intl.NumberFormat("en-US",
-    { style: "currency", currency: "USD",
-      minimumFractionDigits: 2 }).format;
-
   for (let perf of invoice.performances) { 
     volumeCredits += volumeCreditsFor(perf);
 
     // print line for this order
-    result += `  ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience} seats)\n`;
+    result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     totalAmount += amountFor(perf);
   }
 
-  result += `Amount owed is ${format(totalAmount/100)}\n`;
+  result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 }
@@ -77,6 +73,12 @@ function volumeCreditsFor(aPerformance) {
   result += Math.max(aPerformance.audience - 30, 0);
   if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
   return result;
+}
+
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US",
+                      { style: "currency", currency: "USD",
+                        minimumFractionDigits: 2 }).format(aNumber/100);
 }
 
 let result = statement(invoices[0], plays);
